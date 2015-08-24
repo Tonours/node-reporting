21451af9e5f9a0bfba5d3624a5c73c3306ebbbce
Anthony Guimard <anthony@lunaweb.fr>
2015-08-24 14:56:31 +0200

    Fix WeekNumber based on choosen date

--- index.js
+++ index.js
@@ -211,8 +211,9 @@ var extractData = function extractData(startDate, endDate, period, userName) {
 
         events  = {};
         eventsItems = _.groupBy(eventsItems, 'label');
-        events.weekNumber = moment().isoWeeks();
-
+        events.weekNumber = moment(endDate).isoWeeks();
+        console.log(events.weekNumber);
+        
         events.events = _(eventsItems).map(function(g, key) {
           var d;
           return {
